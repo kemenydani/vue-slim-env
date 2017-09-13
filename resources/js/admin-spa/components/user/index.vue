0c39a4b13dd9d08e0fe7b58e89ec6519ae8c326a
kemenydani <kemenydani93@gmail.com>
2017-09-13 15:28:51 +0200

    initial commit

---
+++ resources/js/admin-spa/components/user/index.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div style="border: 1px solid red;" id="user-component-wrapper">
+        Index wrapper
+        <router-view></router-view>
+        Index wrapper
+    </div>
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
